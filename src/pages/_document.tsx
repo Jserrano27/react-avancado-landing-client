000bb69447f1aab501334648bc8d0046fcb969dd
Joaquin Serrano <joaquin.serrano@endurance.com>
2021-01-29 16:40:54 -0300

    upload landing page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,15 @@
-/* istanbul ignore file */
-
+import React from 'react'
 import Document, {
-  DocumentContext,
   Html,
   Head,
   Main,
-  NextScript
+  NextScript,
+  DocumentContext
 } from 'next/document'
+import Analytics from 'components/Analytics'
 import { ServerStyleSheet } from 'styled-components'
 
-export default class MyDocument extends Document {
+export default class NextDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
@@ -38,11 +38,19 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <Html lang="pt-BR">
-        <Head />
+      <Html lang="pt-br">
+        <Head>
+          <link
+            rel="preload"
+            href="/fonts/poppins-v9-latin-regular.woff2"
+            as="font"
+            crossOrigin=""
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
+          <Analytics />
         </body>
       </Html>
     )
